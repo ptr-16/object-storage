9a3734b844ea5de2c1894517302f214713b9f7ba
RIKI TROPER <r0534187895@gmail.com>
2025-06-20 11:24:48 +0300

    I add linter and gitignore...

--- OS-RIKI/minioClient.py
+++ OS-RIKI/minioClient.py
@@ -4,21 +4,36 @@ import random
 import string
 import os
 import logging
+import botocore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
+BUCKET_NAME = os.getenv('BUCKET_NAME') 
 
 s3 = boto3.client(
     's3',
-   endpoint_url='http://minio:9000',
-    aws_access_key_id='admin',
-    aws_secret_access_key='admin123',
+    endpoint_url=MINIO_ENDPOINT,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
 )
 
-BUCKET_NAME='riki'
 OBJECT_KEY='HI.txt'
 Objeckt_Key_Update='HELLO.txt'
 LOCAL_FILE_PATH = os.path.join("temp", Objeckt_Key_Update)
 N=5
+
 logging.basicConfig(level=logging.INFO)
 
+def create_bucket():
+    try:
+        s3.head_bucket(Bucket=BUCKET_NAME)
+    except botocore.exceptions.ClientError:
+        s3.create_bucket(Bucket=BUCKET_NAME)
+    
 def Create_Object():
     """"create a random files"""
     try:
@@ -30,11 +45,24 @@ def Create_Object():
             with open(LOCAL_FILE_PATH, "w") as f:
                 f.write(''.join(random.choices(string.ascii_letters + string.digits, k=80)))
             s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, OBJECT_KEY)
-        logging.info("it uplaod well")
+        logging.info("it uplaoded well")
     except Exception as e:
          logging.info(f"An error occurred: {e}")
-        
-#1.
+
+def create_file_check():
+    """create file to if thr function work well"""
+    try:
+        filname='HI.txt'
+        OBJECT_KEY=filname
+        LOCAL_FILE_PATH =  os.path.join("temp", OBJECT_KEY)
+        with open(LOCAL_FILE_PATH, "w") as f:
+            f.write('HI TO EVERY ONE!!!!')
+        s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, OBJECT_KEY)
+        logging.info("it uplaoded well")
+    except Exception as e:
+         logging.info(f"An error occurred: {e}")
+
+"""1"""
 def List_Exsit_Object():
     """give the list of the Object"""
     try:
@@ -48,7 +76,7 @@ def List_Exsit_Object():
     except Exception as e:
         logging.info(f"An error occurred: {e}")
 
-# 2.
+"""2"""
 def Read_Data_Of_Exist_Object(OBJECT_KEY):
     """read the data from exist object"""
     try:
@@ -60,7 +88,7 @@ def Read_Data_Of_Exist_Object(OBJECT_KEY):
     except Exception as e:
         logging.info(f"An error occurred: {e}")
 
-#3
+"""3"""
 def Remove_Exist_Object(OBJECT_KEY):
     """remove exist object"""
     try:
@@ -69,7 +97,7 @@ def Remove_Exist_Object(OBJECT_KEY):
     except Exception as e:
         logging.info(f"Error deleting object: {e}")    
 
-#4
+"""4"""
 def Update_an_existing_object(Objeckt_Key_Update,LOCAL_FILE_PATH):
     """updata the exist object"""
     try:
@@ -79,8 +107,10 @@ def Update_an_existing_object(Objeckt_Key_Update,LOCAL_FILE_PATH):
     except Exception as e:
         logging.info(f"Error updating object: {e}")
 
-#Summoning the methods
+"""Summoning the methods"""
+create_bucket()
 Create_Object()
+create_file_check()
 List_Exsit_Object()
 Read_Data_Of_Exist_Object(OBJECT_KEY)
 Remove_Exist_Object(OBJECT_KEY)
